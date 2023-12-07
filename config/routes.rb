b0383b721d99bc3b826d22ffee2c27dcfb5d87ea
nishimura321 <146441655+nishimura321@users.noreply.github.com>
2023-12-07 03:28:02 +0000

    [Finish]cart_itemsコントローラ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,21 @@
 Rails.application.routes.draw do
 
-  # 顧客用
+  namespace :public do
+    get 'cart_items/index'
+  end
+  # device顧客用
   # URL /customers/sign_in ...
   devise_for :customers, skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
   }
 
-  # 管理者用
+  # device管理者用
   # URL /admin/sign_in ...
   devise_for :admin, skip: [:registrations, :passwords], controllers: {
     sessions: "admin/sessions"
   }
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   #顧客側
@@ -21,6 +25,8 @@ Rails.application.routes.draw do
     resources :items, only: [:index, :show]
     get 'customers/mypage' => 'customers#show'
     get 'customers/information/edit' => 'customers#edit'
+    get 'customers/confirm_withdrawal' => 'customers#confirm_withdrawal'
+    resources :cart_items, only: [:index]
   end
 
   #管理者側
