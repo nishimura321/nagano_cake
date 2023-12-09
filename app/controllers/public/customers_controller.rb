89c9f95d8fe18fce1af8a285d202132068193c31
nishimura321 <146441655+nishimura321@users.noreply.github.com>
2023-12-09 07:01:35 +0000

    [Finish]マイページ

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -2,9 +2,11 @@ class Public::CustomersController < ApplicationController
   before_action :authenticate_customer!
 
   def show
+    @customer = current_customer
   end
 
   def edit
+    @customer = current_customer
   end
 
   def update
