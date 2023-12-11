6e527031ed41b7ff160db352013258358b3bd2b3
nishimura321 <146441655+nishimura321@users.noreply.github.com>
2023-12-11 06:41:42 +0000

    [Add]退会処理

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -2,7 +2,7 @@
 
 class Public::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
-  before_action :reject_customer, only: [:create]
+  before_action :customer_state, only: [:create]
 
   # GET /resource/sign_in
   # def new
@@ -19,22 +19,8 @@ class Public::SessionsController < Devise::SessionsController
   #   super
   # end
 
-  protected
-
-  def reject_customer
-    @customer = Customer.find_by(email: params[:customer][:email])
-    if @customer
-      if @customer.valid_password?(params[:customer][:password]) && (@customer.is_active == false)
-        flash[:notice] = "退会済みです。再度ご登録をしてご利用ください"
-        redirect_to new_customer_registration_path
-      else
-        flash[:notice] = "項目を入力してください"
-      end
-    else
-      flash[:notice] = "該当するユーザーが見つかりません"
-    end
-  end
-
+  #protected
+  
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
@@ -46,6 +32,27 @@ class Public::SessionsController < Devise::SessionsController
 
   def after_sign_out_path_for(resource)
     root_path
+  end
+
+  private
+  # アクティブであるかを判断するメソッド
+  def customer_state
+    # 【処理内容1】 入力されたemailからアカウントを1件取得
+    customer = Customer.find_by(email: params[:customer][:email])
+    # 【処理内容2】 アカウントを取得できなかった場合、このメソッドを終了する
+    return if customer.nil?
+    # 【処理内容3】 取得したアカウントのパスワードと入力されたパスワードが一致していない場合、このメソッドを終了する
+    return unless customer.valid_password?(params[:customer][:password])
+    # 【処理内容4】 アクティブでない会員に対する処理
+    customer.is_active == false
+    flash[:notice] = "退会済みです。再度ご登録をしてご利用ください"
+    redirect_to new_customer_registration_path
+    else
+      flash[:notice] = "項目を入力してください"
+    end
+    else
+      flash[:notice] = "該当するユーザーが見つかりません"
+    end
   end
 
 end
