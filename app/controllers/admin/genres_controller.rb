99d0d6825506b526cb566bce4ba7533176004e7c
nishimura321 <146441655+nishimura321@users.noreply.github.com>
2023-12-13 06:24:34 +0000

    [Finish]ジャンル一覧

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -2,17 +2,43 @@ class Admin::GenresController < ApplicationController
   before_action :authenticate_admin!
 
   def new
+    @genre = Genre.new
   end
 
   def create
+    @genre = Genre.new(genre_params)
+    if @genre.save
+      flash.now[:notice] = "新規登録が完了しました。"
+      render :index
+    else
+      flash.now[:notice] = "新規登録に失敗しました。"
+      render :index
+    end
   end
 
   def index
+    @genres = Genre.all
+    @genre = Genre.new
   end
 
   def edit
+    @genre = Genre.find(params[:id])
   end
 
   def update
+    @genre = Genre.find(params[:id])
+    if @genre.update(genre_params)
+      flash[:notice] = "変更を保存しました。"
+    redirect_to admin_genres_path
+    else
+      flash.now[:notice] = "変更の保存に失敗しました。"
+      render :edit
+    end
   end
+
+  private
+  def genre_params
+    params.require(:genre).permit(:name)
+  end
+
 end
