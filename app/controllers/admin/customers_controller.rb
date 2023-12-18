d7688c7ef3fe4de6eed68bb3181d162a9fc4ee7c
nishimura321 <146441655+nishimura321@users.noreply.github.com>
2023-12-18 15:23:48 +0000

    [Finish]会員編集画面

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -6,11 +6,27 @@ class Admin::CustomersController < ApplicationController
   end
 
   def show
+    @customer = Customer.find(params[:id])
   end
 
   def edit
+    @customer = Customer.find(params[:id])
   end
 
   def update
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+      flash[:notice] = "変更を保存しました。"
+      redirect_to admin_customer_path(@customer)
+    else
+      flash.now[:notice] = "変更の保存に失敗しました。"
+      render :edit
+    end
   end
+
+  private
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number, :email, :is_active)
+  end
+
 end
