97c5e17bf0a5e6410ff33caff5bff96557b99c22
nishimura321 <146441655+nishimura321@users.noreply.github.com>
2023-12-20 07:02:19 +0000

    [Finish]カートに入れる

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -2,9 +2,14 @@ class Public::CartItemsController < ApplicationController
 before_action :authenticate_customer!
 
   def create
+    cart_item = CartItem.new(cart_item_params)
+    cart_item.save
+    redirect_to cart_items_path
   end
 
   def index
+    @items = Item.all(params[:id])
+    @cart_items = CartItem.all(current_customer.id)
   end
 
   def update
@@ -14,6 +19,12 @@ before_action :authenticate_customer!
   end
 
   def destroy_all
+  end
+
+  private
+ 
+  def cart_item_params
+    params.require(:cart_item).permit(:item_id, :amount)
   end
 
 end
