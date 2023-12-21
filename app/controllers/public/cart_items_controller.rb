d2b27552c53c73e4a817a80f848860b2daf43295
nishimura321 <146441655+nishimura321@users.noreply.github.com>
2023-12-21 12:45:07 +0000

    カート商品途中

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -8,21 +8,40 @@ before_action :authenticate_customer!
   end
 
   def index
-    @items = Item.all(params[:id])
-    @cart_items = CartItem.all(current_customer.id)
+    @cart_items = current_customer.cart_items
+    #合計を求める
+    @total_price = 0
+    @cart_items.each do |cart_item|
+      @total_price += item.subtotal
+    end
   end
 
   def update
+    @cart_item = CartItem.find(params[:id])
+    if @cart_item.update(cart_item_params)
+      flash.now[:notice] = "変更を保存しました。"
+      render :index
+    else
+      flash.now[:notice] = "変更の保存に失敗しました。"
+      render :index
+    end
   end
 
   def destroy
+    @cart_item = CartItem.find(params[:id])
+    @cart_item.destroy
+    flash.now[:notice] = "削除しました。"
+    render :index
   end
 
   def destroy_all
+    current_customer.cart_items.destroy_all
+    flash[:notice] = "カートを空にしました"
+    render :index
   end
 
   private
- 
+
   def cart_item_params
     params.require(:cart_item).permit(:item_id, :amount)
   end
