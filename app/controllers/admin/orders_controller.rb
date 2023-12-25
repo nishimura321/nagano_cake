fb7933a0f2332bc07a55f44fae9bf8e87ddf295d
nishimura321 <146441655+nishimura321@users.noreply.github.com>
2023-12-25 15:40:29 +0000

    [finish]注文情報

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -3,11 +3,12 @@ before_action :authenticate_admin!
 
   def show
     @order = Order.find(params[:id])
-    @order_details = OrderDetail.find(params[:id])
+    @order_details = @order.order_details
     #合計を求める
     @total_price = 0
     @order_detail.each do |order_detail|
       @total_price += order_detail.subtotal
+    end
   end
 
   #def update
--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -2,9 +2,13 @@ class Public::OrdersController < ApplicationController
   before_action :authenticate_customer!
 
   def new
+    @order = Order.new
   end
 
   def create
+    order = Order.new(order_params)
+    order.save
+    redirect_to orders_confirm_path
   end
 
   def index
@@ -19,4 +23,9 @@ class Public::OrdersController < ApplicationController
   def thanks
   end
 
+  private
+  def order_params
+    params.require(:order).permit(:postal_code, :address, :name, :shipping_fee, :total_payment, :payment_methode)
+  end
+  
 end
