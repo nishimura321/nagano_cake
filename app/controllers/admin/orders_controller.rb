708a1a88eacd76bd1a88dccb43aa6780d0cb5583
nishimura321 <146441655+nishimura321@users.noreply.github.com>
2023-12-25 07:38:38 +0000

    [Finish]注文履歴詳細

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -2,9 +2,15 @@ class Admin::OrdersController < ApplicationController
 before_action :authenticate_admin!
 
   def show
+    @order = Order.find(params[:id])
+    @order_details = OrderDetail.find(params[:id])
+    #合計を求める
+    @total_price = 0
+    @order_detail.each do |order_detail|
+      @total_price += order_detail.subtotal
   end
 
-  def update
-  end
+  #def update
+  #end
 
 end
