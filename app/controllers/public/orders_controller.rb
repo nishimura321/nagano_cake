01966bb3e2dad5cc2f8874b43d84bbbab5e57ebc
nishimura321 <146441655+nishimura321@users.noreply.github.com>
2023-12-26 14:35:51 +0000

    [Finish]注文情報確認

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -2,22 +2,13 @@ class Public::OrdersController < ApplicationController
   before_action :authenticate_customer!
 
   def new
-    @order = Order.new(customer_id: current_customer.id)
+    @order = Order.new
   end
 
   def create
-    customer = Customer.find(current_customer.id)
-    @order = Order.new(
-    postal_code: customer.postal_code,
-    address: customer.address,
-    name: "#{customer.last_name} #{customer.first_name}"
-    )
-
-    if @order.save
-      redirect_to orders_confirm_path
-    else
-      render :new
-    end
+    order = Order.new(order_params)
+    order.save
+    redirect_to oeders_thanks_path
   end
 
   def index
@@ -27,12 +18,18 @@ class Public::OrdersController < ApplicationController
   end
 
   def confirm
+    @order = Order.new(order_params)
+    @order.postal_code = current_customer.postal_code
+    @order.address = current_customer.address
+    @order.name = current_customer.last_name + current_customer.first_name
     @cart_items = current_customer.cart_item
     #合計を求める
     @total_price = 0
     @cart_items.each do |cart_item|
       @total_price += cart_item.subtotal
     end
+    @order.shipping_fee = 800
+    @order.total_payment = @order.shipping_fee + @total_price
   end
 
   def thanks
@@ -40,7 +37,7 @@ class Public::OrdersController < ApplicationController
 
   private
   def order_params
-    params.require(:order).permit(:postal_code, :address, :name, :shipping_fee, :total_payment, :payment_methode)
+    params.require(:order).permit(:postal_code, :address, :name, :shipping_fee, :total_payment, :payment_method)
   end
 
 end
