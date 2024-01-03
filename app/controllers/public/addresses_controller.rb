7c7c55c77159fbb0227278730a0467dbe6f98793
nishimura321 <146441655+nishimura321@users.noreply.github.com>
2024-01-03 15:51:52 +0000

    [Finsh]配送先

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -2,18 +2,50 @@ class Public::AddressesController < ApplicationController
 before_action :authenticate_customer!
 
   def create
+    @address = Address.new(address_params)
+    if @address.save
+      flash[:notice] = "新規登録が完了しました。"
+      redirect_to addresses_path
+    else
+      flash.now[:notice] = "新規登録に失敗しました。"
+      render :index
+    end
   end
 
   def index
+    @address = Address.new
+    @addresses = Address.where(customer_id: current_customer.id)
   end
 
   def edit
+    @address = Address.find(current_customer.id)
   end
 
   def update
+    @address = Address.find(current_customer.id)
+    if @address.update(address_params)
+      flash[:notice] = "編集が完了しました。"
+      redirect_to addresses_path
+    else
+      flash.now[:notice] = "編集が失敗しました。"
+      render :edit
+    end
   end
 
   def destroy
+    @address = Address.find(current_customer.id)
+    if @address.destroy
+      flash[:notice] = "削除しました。"
+      redirect_to addresses_path
+    else
+      flash.now[:notice] = "削除に失敗しました。"
+      render :index
+    end
+  end
+
+private
+  def address_params
+    params.require(:address).permit(:customer_id, :postal_code, :address, :name)
   end
 
 end
--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -32,11 +32,23 @@ class Public::OrdersController < ApplicationController
 
   def confirm
     @order = Order.new(order_params)
-    @order.postal_code = current_customer.postal_code
-    @order.address = current_customer.address
-    @order.name = current_customer.last_name + current_customer.first_name
-    @cart_items = current_customer.cart_item
+    if params[:order][:select_address] == "0"
+      @order.postal_code = current_customer.postal_code
+      @order.address = current_customer.address
+      @order.name = current_customer.last_name + current_customer.first_name
+      @cart_items = current_customer.cart_item
+    elsif params[:order][:select_address] == "1"
+      @address = Address.find(params[:order][:address_id])
+      @order.postal_code = @address.postal_code
+      @order.address = @address.address
+      @order.name = @address.name
+    elsif params[:order][:select_address] == "2"
+      @order.postal_code = params[:order][:postal_code]
+      @order.address = params[:order][:address]
+      @order.name = params[:order][:name]
+    end
     #合計を求める
+    @cart_items = current_customer.cart_item
     @total_price = 0
     @cart_items.each do |cart_item|
       @total_price += cart_item.subtotal
